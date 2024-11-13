29155af8b8d0cca2b2690bd7aa85ec9069808b2c
Rafale2000 <loick1morneau@icloud.com>
2024-11-13 08:43:34 +0100

    changement du package de bdd a BDD

--- app/src/main/java/com/example/questease/Controller/ChoseATrouverPrixJusteHandler.java
+++ app/src/main/java/com/example/questease/Controller/ChoseATrouverPrixJusteHandler.java
@@ -6,7 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.example.questease.Model.bdd.ChoseATrouverPrixJuste;
+import com.example.questease.Model.BDD.ChoseATrouverPrixJuste;
 
 public class ChoseATrouverPrixJusteHandler extends MyDatabaseHelper {
     private SQLiteOpenHelper dbHelper;
