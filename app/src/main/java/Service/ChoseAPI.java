a574092ed9e9e669820f70cf89c1f5c02d5f39cc
Rafale2000 <loick1morneau@icloud.com>
2024-11-13 08:42:58 +0100

    changement d'un import

--- app/src/main/java/Service/ChoseAPI.java
+++ app/src/main/java/Service/ChoseAPI.java
@@ -3,6 +3,8 @@ package Service;
 
 //TODO Reprendre les paramètrs du manifest
 
+import com.example.questease.Model.BDD.ChoseATrouverPrixJuste;
+
 import java.util.List;
 
 import retrofit2.Call;
@@ -10,8 +12,8 @@ import retrofit2.http.GET;
 
 public interface ChoseAPI {
     @GET("/chose")
-    Call<List<Model.BDD.ChoseATrouverPrixJuste>> getMotChose();
+    Call<List<ChoseATrouverPrixJuste>> getMotChose();
 
     @GET("/chose/random")
-    Call<Model.BDD.ChoseATrouverPrixJuste> getChoseRandom();
+    Call<ChoseATrouverPrixJuste> getChoseRandom();
 }
