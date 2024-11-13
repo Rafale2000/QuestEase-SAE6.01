d1f9e5ed0e7a1f0a7a60e90a666c851eaf1c2d88
Rafale2000 <loick1morneau@icloud.com>
2024-11-13 14:17:03 +0100

    API de la classe Indice

---
+++ app/src/main/java/Service/IndiceAPI/IndiceCallBack.java
@@ -0,0 +1,9 @@
+package Service.IndiceAPI;
+
+import com.example.questease.Model.BDD.Indice;
+
+
+public interface IndiceCallBack {
+    void OnIndiceReceived(Indice indice);
+    void OnFailure(String errorMessage);
+}
