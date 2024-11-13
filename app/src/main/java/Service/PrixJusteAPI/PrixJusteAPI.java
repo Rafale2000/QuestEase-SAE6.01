b9fec18c665c3d72eeb71b4e8f96d353ef4add0c
Rafale2000 <loick1morneau@icloud.com>
2024-11-13 14:16:28 +0100

    API de la classe Prix Juste

--- app/src/main/java/Service/PrixJusteAPI/PrixJusteAPI.java
+++ app/src/main/java/Service/PrixJusteAPI/PrixJusteAPI.java
@@ -1,4 +1,4 @@
-package Service;
+package Service.PrixJusteAPI;
 
 public interface PrixJusteAPI {
 }
--- app/src/main/java/Service/PrixJusteAPI/PrixJusteAdapter.java
+++ app/src/main/java/Service/PrixJusteAPI/PrixJusteAdapter.java
@@ -1,4 +1,4 @@
-package Service;
+package Service.PrixJusteAPI;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
