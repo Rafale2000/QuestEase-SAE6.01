c53889e09a288ad9c738182d0b7e6b6588d10cc7
Rafale2000 <loick1morneau@icloud.com>
2024-11-18 14:54:32 +0100

    ajout d'une variable indice de type Indice pour regler un problème

--- app/src/main/java/com/example/questease/Model/BDD/MotCryptex.java
+++ app/src/main/java/com/example/questease/Model/BDD/MotCryptex.java
@@ -1,21 +1,44 @@
 package com.example.questease.Model.BDD;
 
+import com.example.questease.Model.BDD.Indice;
+
 public class MotCryptex {
     private int id;
     private String mot;
     private int diff;
-    private int idIndice;
+    private int idIndice; // Ajouté
+    private Indice indice;
+
+    public MotCryptex(int id, String mot, int diff, Indice indice) {
+        this.id = id;
+        this.mot = mot;
+        this.diff = diff;
+        this.indice = indice;
+        this.idIndice = indice != null ? indice.getId() : 0; // Si l'indice est passé
+    }
 
     public MotCryptex(int id, String mot, int diff, int idIndice) {
         this.id = id;
         this.mot = mot;
         this.diff = diff;
         this.idIndice = idIndice;
+        this.indice = null; // Pour éviter toute confusion
+    }
 
+    public int getIdIndice() {
+        return idIndice;
+    }
+
+    public void setIdIndice(int idIndice) {
+        this.idIndice = idIndice;
     }
 
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getMot() {
@@ -34,15 +57,13 @@ public class MotCryptex {
         this.diff = diff;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public Indice getIndice() {
+        return indice;
     }
 
-    public int getIdIndice() {
-        return idIndice;
+    public void setIndice(Indice indice) {
+        this.indice = indice;
     }
 
-    public void setIdIndice(int idIndice) {
-        this.idIndice = idIndice;
-    }
+    // Autres getters et setters
 }
