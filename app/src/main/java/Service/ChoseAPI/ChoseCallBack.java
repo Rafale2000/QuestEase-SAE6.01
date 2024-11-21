70995215b9844a69199647ef93087ba377ac4934
Rafale2000 <loick1morneau@icloud.com>
2024-11-21 11:20:41 +0100

    changement dans l'api de la classe chose

---
+++ app/src/main/java/Service/ChoseAPI/ChoseCallBack.java
@@ -0,0 +1,8 @@
+package Service.ChoseAPI;
+
+import com.example.questease.Model.BDD.ChoseATrouverPrixJuste;
+
+public interface ChoseCallBack {
+    void onChoseReceived(ChoseATrouverPrixJuste chose);
+    void onFailure(String errorMessage);
+}
