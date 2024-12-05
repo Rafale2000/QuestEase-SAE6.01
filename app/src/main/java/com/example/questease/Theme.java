8cff8fddb318257d34bb8c643f6735f71d3a45e4
Hugo Faes <hugo.faes444@gmail.com>
2024-12-05 18:45:51 +0100

    finitialisation de SM

--- app/src/main/java/com/example/questease/Theme.java
+++ app/src/main/java/com/example/questease/Theme.java
@@ -17,7 +17,6 @@ import android.widget.TextView;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.res.ResourcesCompat;
 
 
@@ -194,8 +193,11 @@ public abstract class Theme extends AppCompatActivity {
             intentgame = new Intent(this, RotatingPictures.class);
         } else if ("rotating_pictures2".equals(message)) {
             intentgame = new Intent(this, RotatingPictures2.class);
-        } else if ("menteur1".equals(message) || "menteur2".equals(message)) {
+        } else if ("menteur1".equals(message)) {
             intentgame = new Intent(this, Sincere_Menteur.class);
+        }
+          else if("menteur2".equals(message)){
+            intentgame = new Intent(this, Sincere_Menteur2.class);
         } else if ("cryptex".equals(message)) {
             // intentgame = new Intent(Lobby.this, Cryptex.class);
         } else {
