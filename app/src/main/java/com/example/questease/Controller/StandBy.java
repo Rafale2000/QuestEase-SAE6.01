be0ae8adb0efd5befb93d0e3df3f336ce7d13cdb
Arirou <sprozdut@gmail.com>
2024-12-05 21:43:37 +0100

    Standby

---
+++ app/src/main/java/com/example/questease/Controller/StandBy.java
@@ -0,0 +1,13 @@
+package com.example.questease.Controller;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.questease.R;
+
+public class StandBy extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.standby);
+    }
+}
