f9ab3a7d55047e9d96c63408fd362bcbdc3db9bf
Rafale2000 <loick1morneau@icloud.com>
2025-01-30 15:19:30 +0100

    refractor de la page pour réduire le nombre d'erreur

--- app/src/main/java/com/example/questease/TrouveLeSon2.java
+++ app/src/main/java/com/example/questease/TrouveLeSon2.java
@@ -13,7 +13,6 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.IBinder;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
